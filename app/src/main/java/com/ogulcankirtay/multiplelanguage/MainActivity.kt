03f8b37a11a675212df9ebff5a7730166fff127c
ogulcan <ogulcankirtay8@gmail.com>
2024-11-13 00:32:10 +0300

    multiple language support string added for app

--- app/src/main/java/com/ogulcankirtay/multiplelanguage/MainActivity.kt
+++ app/src/main/java/com/ogulcankirtay/multiplelanguage/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ogulcankirtay.multiplelanguage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnShowToast: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnShowToast=findViewById(R.id.button)
+        btnShowToast.setOnClickListener{
+            Toast.makeText(applicationContext,R.string.toast_message,Toast.LENGTH_LONG).show()
+        }
     }
 }
