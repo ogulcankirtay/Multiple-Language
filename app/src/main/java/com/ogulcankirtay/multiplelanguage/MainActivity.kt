101690e520184dcb107679cf749fb9c42f9fa7cd
ogulcan <ogulcankirtay8@gmail.com>
2024-11-18 23:19:23 +0300

    Added simple example for different screen sizes and landscape mode

--- app/src/main/java/com/ogulcankirtay/multiplelanguage/MainActivity.kt
+++ app/src/main/java/com/ogulcankirtay/multiplelanguage/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ogulcankirtay.multiplelanguage
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,12 +9,24 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnShowToast: Button
+    lateinit var btnGoPage: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnShowToast=findViewById(R.id.button)
+        btnGoPage=findViewById(R.id.buttonGo)
         btnShowToast.setOnClickListener{
             Toast.makeText(applicationContext,R.string.toast_message,Toast.LENGTH_LONG).show()
+
+            supportActionBar!!.setDisplayShowCustomEnabled(true)
+            supportActionBar!!.setLogo(R.mipmap.ic_launcher)
+            supportActionBar!!.setDisplayUseLogoEnabled(true)
+
+        }
+        btnGoPage.setOnClickListener{
+            val intent=Intent(this,diffrentScreenSizesActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/ogulcankirtay/multiplelanguage/diffrentScreenSizesActivity.kt
@@ -0,0 +1,11 @@
+package com.ogulcankirtay.multiplelanguage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class diffrentScreenSizesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diffrent_screen_sizes)
+    }
+}
